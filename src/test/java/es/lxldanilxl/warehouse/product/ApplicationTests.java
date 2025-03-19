57c198ab554cd4dd2d1d3c6d52d6382dec9ca0af
Rogelio Daniel Franco Martínez <contacto@lxldanilxl.es>
2025-03-19 11:03:16 +0100

    Añadido entorno test a la clase test

--- src/test/java/es/lxldanilxl/warehouse/product/ApplicationTests.java
+++ src/test/java/es/lxldanilxl/warehouse/product/ApplicationTests.java
@@ -2,8 +2,10 @@ package es.lxldanilxl.warehouse.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTests {
 
 	@Test
