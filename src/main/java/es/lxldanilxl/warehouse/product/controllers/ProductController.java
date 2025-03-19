07b9919cceb907c0f92928455688f98c3f51d216
Rogelio Daniel Franco Martínez <contacto@lxldanilxl.es>
2025-03-19 11:48:27 +0100

    Added more endpoints

--- src/main/java/es/lxldanilxl/warehouse/product/controllers/ProductController.java
+++ src/main/java/es/lxldanilxl/warehouse/product/controllers/ProductController.java
@@ -1,13 +1,24 @@
 package es.lxldanilxl.warehouse.product.controllers;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.lxldanilxl.warehouse.product.model.dtos.ProductRequest;
+import es.lxldanilxl.warehouse.product.model.dtos.ProductResponse;
 import es.lxldanilxl.warehouse.product.services.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/product")
@@ -15,9 +26,32 @@ import lombok.RequiredArgsConstructor;
 public class ProductController {
     private final ProductService productService;
 
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void addProduct(ProductRequest productRequest) {
+    public void addProduct(@RequestBody ProductRequest productRequest) {
         productService.addProduct(productRequest);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProduct(@RequestParam Long id) {
+        return ResponseEntity.ok(productService.getProduct(id));
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateProduct(@PathVariable Long id, @RequestBody ProductRequest productRequest) {
+        productService.updateProduct(id, productRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
     }
 
 }
